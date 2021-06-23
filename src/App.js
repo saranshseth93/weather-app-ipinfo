e0c573ea70ad9997d4929b3cd7c4dfe84e00dd59
Saransh <saranshs@nookal.com>
2021-06-23 12:21:50 +0530

    Bug fixes and finalising

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
 import env from "react-dotenv";
+import iconDay from "./assets/amcharts_weather_icons_1.0.0/animated/day.svg";
+import iconNight from "./assets/amcharts_weather_icons_1.0.0/animated/night.svg";
+import iconCloudDay from "./assets/amcharts_weather_icons_1.0.0/animated/cloudy-day-1.svg";
+import iconCloudNight from "./assets/amcharts_weather_icons_1.0.0/animated/cloudy-night-1.svg";
+import iconRainy from "./assets/amcharts_weather_icons_1.0.0/animated/rainy-1.svg";
+import iconSnow from "./assets/amcharts_weather_icons_1.0.0/animated/snowy-1.svg";
+import iconThunder from "./assets/amcharts_weather_icons_1.0.0/animated/thunder.svg";
+import moment from "moment";
 
 const api = {
   key: env.API_KEY,
@@ -11,75 +19,132 @@ function App() {
   const [weather, setWeather] = useState({});
   const [city, setCity] = useState("");
   const [country, setCountry] = useState("");
-
-  fetch("https://ipinfo.io?token=75832bdf153bdc")
-    .then(res => res.json())
-    .then(result => {
-      if (result !== "undefined" || result !== "") {
-        setCity(result.city);
-        setCountry(result.country);
-      }
-    });
+  const [currentDate, setCurrentDate] = useState("");
+  const [currentTime, setCurrentTime] = useState("");
+  const [icon, setIcon] = useState("");
 
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
-    // Update the document title using the browser API
     loadTemp();
   }, [city]); // Whenever city is changed
 
-  const loadTemp = () => {
-    fetch(
-      `${api.base}weather?q=${city}, ${country} &units=metric&APPID=${api.key}`
-    )
+  const initialLoad = () => {
+    console.log("initial");
+    fetch("https://ipinfo.io?token=75832bdf153bdc")
       .then(res => res.json())
       .then(result => {
-        setWeather(result);
-        setQuery("");
+        if (result !== "undefined" || result !== "") {
+          console.log(result);
+          let date = new Date();
+          let locDate = date.toLocaleString("de-DE", {
+            timeZone: result.timezone
+          });
+
+          let currDate = locDate.split(",")[0];
+          let currTime = locDate.split(",")[1].trim();
+
+          let day = moment(currDate, "DD.MM.YYYY").format("dddd, DD MMM YYYY");
+          let time = moment(currTime, "HH:mm:ss").format("hh:mmA");
+
+          setCurrentDate(day);
+          setCurrentTime(time);
+          setCity(result.city);
+          setCountry(result.country);
+        }
       });
   };
 
-  const search = evt => {
-    if (evt.key === "Enter") {
+  const loadIcon = result => {
+    if (
+      result.weather[0].main === "Clear" &&
+      currentTime.indexOf("AM") !== -1
+    ) {
+      setIcon(iconDay);
+    } else if (
+      result.weather[0].main === "Clear" &&
+      currentTime.indexOf("PM") !== -1
+    ) {
+      setIcon(iconNight);
+    } else if (
+      (result.weather[0].main === "Cloudy" ||
+        result.weather[0].main === "Haze" ||
+        result.weather[0].main === "Clouds") &&
+      currentTime.indexOf("AM") !== -1
+    ) {
+      setIcon(iconCloudDay);
+    } else if (
+      (result.weather[0].main === "Cloudy" ||
+        result.weather[0].main === "Haze" ||
+        result.weather[0].main === "Clouds") &&
+      currentTime.indexOf("PM") !== -1
+    ) {
+      setIcon(iconCloudNight);
+    } else if (
+      result.weather[0].main === "Mist" ||
+      result.weather[0].main === "Rainy"
+    ) {
+      setIcon(iconRainy);
+    } else if (
+      result.weather[0].main === "Snow" ||
+      result.weather[0].main === "Hail"
+    ) {
+      setIcon(iconSnow);
+    } else if (
+      result.weather[0].main === "Thunder" ||
+      result.weather[0].main === "Thunderstorm"
+    ) {
+      setIcon(iconThunder);
+    }
+  };
+
+  const loadTemp = () => {
+    if (query === "") {
+      console.log(query);
+      if (city !== "") {
+        fetch(
+          `${api.base}weather?q=${city}, ${country} &units=metric&APPID=${api.key}`
+        )
+          .then(res => res.json())
+          .then(result => {
+            setWeather(result);
+            // setQuery("");
+
+            loadIcon(result);
+          });
+      } else {
+        initialLoad();
+      }
+    } else {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
         .then(result => {
-          setWeather(result);
-          setQuery("");
+          if (result.message !== "city not found") {
+            setWeather(result);
+            // setQuery("");
+
+            setCurrentDate(
+              moment()
+                .utcOffset(Math.round(result.timezone / 60))
+                .format("dddd, DD MMM YYYY")
+            );
+            setCurrentTime(
+              moment()
+                .utcOffset(Math.round(result.timezone / 60))
+                .format("hh:mmA")
+            );
+            console.log(result);
+            loadIcon(result);
+          } else {
+            setWeather(result);
+          }
         });
     }
   };
 
-  const dateBuilder = d => {
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
-    ];
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`;
+  const search = evt => {
+    if (evt.key === "Enter") {
+      loadTemp();
+    }
   };
 
   return (
@@ -98,22 +163,42 @@ function App() {
           <input
             type="text"
             className="search-bar"
-            placeholder="Search..."
+            placeholder="Search City Name..."
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
           />
         </div>
+        {weather.message === "city not found" && (
+          <div className="error-box">
+            <p>City Not Found</p>
+          </div>
+        )}
         {typeof weather.main !== "undefined" ? (
           <div>
             <div className="location-box">
               <div className="location">
                 {weather.name}, {weather.sys.country}
               </div>
-              <div className="date">{dateBuilder(new Date())}</div>
+              <div className="date">
+                {currentDate} {currentTime}
+              </div>
             </div>
             <div className="weather-box">
-              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="temp">
+                {typeof icon !== "undefined" ? (
+                  <div className="weather-icon">
+                    <img src={icon} alt="Weather icon" />
+                  </div>
+                ) : (
+                  ""
+                )}
+                {Math.round(weather.main.temp)}°c
+                <div className="info-box">
+                  <p>Feels like {Math.round(weather.main.feels_like)}°c</p>
+                  <p>Humidity {Math.round(weather.main.humidity)}%</p>
+                </div>
+              </div>
               <div className="weather">{weather.weather[0].main}</div>
             </div>
           </div>
